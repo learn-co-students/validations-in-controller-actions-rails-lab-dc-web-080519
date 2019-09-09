c2a8a49bc82920186d3310f95a2154a427cf33ce
German Garcia Gonzalez <ggarciag.business@outlook.com>
2019-09-09 09:51:46 -0400

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,6 @@
 class AuthorsController < ApplicationController
+
+
   def show
     @author = Author.find(params[:id])
   end
@@ -7,10 +9,16 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create!(author_params)
+    @author = Author.new(author_params)
 
-    redirect_to author_path(@author)
+    if @author.save
+
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
+
 
   private
 
